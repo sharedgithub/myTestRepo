f08fe3a43c35b608f52c6c999a025e16bd513de1
end ralsd <end.rald@gmail.com>
2019-01-26 12:34:54 +0530

    rest files

---
+++ restAss/resources/StepDef.java
@@ -0,0 +1,10 @@
+package resources;
+
+import io.restassured.*;
+import cucumber.api.java.en.Given;
+public class StepDef {
+	
+	@Given("I want to write a step with precondition")
+	public void givenHain() {
+	}
+}
