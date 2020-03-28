5a92883dec4923bcdd9475d71626cfed8a490768
sharedgithub <aksr30587@gmail.com>
2020-03-28 14:40:15 +0530

    Create C_PrototypePattern.java

---
+++ src/design/pattern/C_PrototypePattern.java
@@ -0,0 +1,16 @@
+package main.designPatterns;
+
+public class C_PrototypePatern {
+
+    public static void main(String[] args) {
+        MachineImage linux=new MachineImage("Linux","x86");
+        MachineImage windows=new MachineImage("windows","x86");
+        MachineImage newLinux=linux.clone();
+    }
+}
+
+class MachineImage implements Cloneable{
+    String os;      String type;
+    MachineImage(String os,String type)     {this.os=os;     this.type=type; }
+    @Override   public MachineImage clone()     { return new MachineImage(os,type);}
+}
