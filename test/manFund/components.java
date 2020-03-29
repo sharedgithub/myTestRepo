cc4a01c4de6592b6d7823f01544a0c5108058fad
sharedgithub <aksr30587@gmail.com>
2020-03-29 17:20:33 +0530

    Create components.java

---
+++ test/manFund/components.java
@@ -0,0 +1,44 @@
+spring helps to write business logic without worrying about writing log of boiler plate code or implementing design pattern
+
+IOC (inversion of control) provoides mechanism for dependency injection
+
+ApplicationContext is a wrapper for the beanFactory class which provides IOC capabilities
+
+spring intializer : start.spring.io
+
+
+
+Linux p2
+	commands
+	shell script
+	awk sed
+Networking
+	
+Java  P1
+	core java
+	Data structures 
+	Algorithms searching and sorting
+	Interview Programs
+	Design patterns 		
+	Mocing					
+	Spring boot and spring	
+
+Python p3
+SQL p3
+
+Automatin  P1
+	UI 
+		selenium
+		locators
+	API
+		restassured
+	Framework
+
+Interview Questions   P1
+	Managerial Questions		#6
+	
+Docker p4 
+
+Jenkins p4
+
+AWS p5
